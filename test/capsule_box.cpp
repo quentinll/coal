c8f379344dcf1e27627aed69c59d15499cac5de6
quentinll <quentin.le-lidec@inria.fr>
2025-04-03 16:54:48 +0200

    Remove obsolete capsule-box test files and consolidate into a single test file.

--- test/capsule_box.cpp
+++ test/capsule_box.cpp
@@ -1,7 +1,7 @@
 /*
  * Software License Agreement (BSD License)
  *
- *  Copyright (c) 2014-2015, CNRS-LAAS and AIST
+ *  Copyright (c) 2014-2016, CNRS-LAAS and AIST
  *  All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
@@ -39,8 +39,6 @@
 
 #define CHECK_CLOSE_TO_0(x, eps) BOOST_CHECK_CLOSE((x + 1.0), (1.0), (eps))
 
-#include "utility.h"
-
 #include <cmath>
 #include "coal/distance.h"
 #include "coal/math/transform.h"
@@ -48,10 +46,12 @@
 #include "coal/collision_object.h"
 #include "coal/shape/geometric_shapes.h"
 
+#include "utility.h"
+
 using coal::Scalar;
 
 BOOST_AUTO_TEST_CASE(distance_capsule_box) {
-  typedef coal::shared_ptr<coal::CollisionGeometry> CollisionGeometryPtr_t;
+  using coal::CollisionGeometryPtr_t;
   // Capsule of radius 2 and of height 4
   CollisionGeometryPtr_t capsuleGeometry(new coal::Capsule(2., 4.));
   // Box of size 1 by 2 by 4
@@ -61,19 +61,74 @@ BOOST_AUTO_TEST_CASE(distance_capsule_box) {
   coal::DistanceRequest distanceRequest(true, 0, 0);
   coal::DistanceResult distanceResult;
 
-  // Rotate capsule around y axis by pi/2 and move it behind box
-  coal::Transform3s tf1(
-      coal::makeQuat(sqrt(Scalar(2)) / 2, 0, sqrt(Scalar(2)) / 2, 0),
-      coal::Vec3s(Scalar(-10.), Scalar(0.8), Scalar(1.5)));
+  // Test case 1: Capsule to the right of box
+  coal::Transform3s tf1(coal::Vec3s(3., 0, 0));
   coal::Transform3s tf2;
   coal::CollisionObject capsule(capsuleGeometry, tf1);
   coal::CollisionObject box(boxGeometry, tf2);
 
   // test distance
+  coal::distance(&capsule, &box, distanceRequest, distanceResult);
+  // Nearest point on capsule
+  coal::Vec3s o1(distanceResult.nearest_points[0]);
+  // Nearest point on box
+  coal::Vec3s o2(distanceResult.nearest_points[1]);
+  BOOST_CHECK_CLOSE(distanceResult.min_distance, 0.5, 1e-1);
+  BOOST_CHECK_CLOSE(o1[0], 1.0, 1e-1);
+  CHECK_CLOSE_TO_0(o1[1], 1e-1);
+  BOOST_CHECK_CLOSE(o2[0], 0.5, 1e-1);
+  CHECK_CLOSE_TO_0(o2[1], 1e-1);
+
+  // Test case 2: Capsule above box
+  tf1 = coal::Transform3s(coal::Vec3s(0., 0., 8.));
+  capsule.setTransform(tf1);
+
+  // test distance
   distanceResult.clear();
   coal::distance(&capsule, &box, distanceRequest, distanceResult);
-  coal::Vec3s o1 = distanceResult.nearest_points[0];
-  coal::Vec3s o2 = distanceResult.nearest_points[1];
+  o1 = distanceResult.nearest_points[0];
+  o2 = distanceResult.nearest_points[1];
+
+  BOOST_CHECK_CLOSE(distanceResult.min_distance, 2.0, 1e-1);
+  CHECK_CLOSE_TO_0(o1[0], 1e-1);
+  CHECK_CLOSE_TO_0(o1[1], 1e-1);
+  BOOST_CHECK_CLOSE(o1[2], 4.0, 1e-1);
+
+  CHECK_CLOSE_TO_0(o2[0], 1e-1);
+  CHECK_CLOSE_TO_0(o2[1], 1e-1);
+  BOOST_CHECK_CLOSE(o2[2], 2.0, 1e-1);
+
+  // Test case 3: Rotated capsule behind box
+  tf1.setTranslation(coal::Vec3s(-10., 0., 0.));
+  tf1.setQuatRotation(
+      coal::makeQuat(sqrt(Scalar(2)) / 2, 0, sqrt(Scalar(2)) / 2, 0));
+  capsule.setTransform(tf1);
+
+  // test distance
+  distanceResult.clear();
+  coal::distance(&capsule, &box, distanceRequest, distanceResult);
+  o1 = distanceResult.nearest_points[0];
+  o2 = distanceResult.nearest_points[1];
+
+  BOOST_CHECK_CLOSE(distanceResult.min_distance, 5.5, 1e-1);
+  BOOST_CHECK_CLOSE(o1[0], -6, 1e-2);
+  CHECK_CLOSE_TO_0(o1[1], 1e-1);
+  CHECK_CLOSE_TO_0(o1[2], 1e-1);
+  BOOST_CHECK_CLOSE(o2[0], -0.5, 1e-2);
+  CHECK_CLOSE_TO_0(o2[1], 1e-1);
+  CHECK_CLOSE_TO_0(o2[2], 1e-1);
+
+  // Test case 4: Rotated capsule behind box with offset
+  tf1 = coal::Transform3s(
+      coal::makeQuat(sqrt(Scalar(2)) / 2, 0, sqrt(Scalar(2)) / 2, 0),
+      coal::Vec3s(Scalar(-10.), Scalar(0.8), Scalar(1.5)));
+  capsule.setTransform(tf1);
+
+  // test distance
+  distanceResult.clear();
+  coal::distance(&capsule, &box, distanceRequest, distanceResult);
+  o1 = distanceResult.nearest_points[0];
+  o2 = distanceResult.nearest_points[1];
 
   BOOST_CHECK_CLOSE(distanceResult.min_distance, 5.5, 1e-2);
   BOOST_CHECK_CLOSE(o1[0], -6, 1e-2);
